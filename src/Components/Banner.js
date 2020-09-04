06a70f6f9439827fd99cda4746fc1336e5c8ddb2
newton <chiakajunior@yahoo.com>
2020-09-05 00:54:42 +0100

    added bootstrap carousel to banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,13 +1,44 @@
-import React from 'react' ;
+import React, {useState} from 'react' ;
 import '../cssModules/Home.css';
+import Carousel from 'react-bootstrap/Carousel';
 import image1 from '../images/image1.jpg';
 
 
 const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
     return (
         <div className='banner'>
-        <img src = {image1} alt ='banner'/>
-        </div>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image1}
+          alt="First slide"
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image1}
+          alt="Second slide"
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image1}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+       </div>
     )
 }
 
